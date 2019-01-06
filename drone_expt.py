d05b3ea927928e7ac3d63cc887523ad4b1737d54
Sourav <souravdeyone@gmail.com>
2019-01-07 05:40:34 +0800

    Replaced locationglobal with LGRelative

--- drone_expt.py
+++ drone_expt.py
@@ -49,7 +49,7 @@ print "Take off complete"
 #Travel to another coordinate for 50 seconds
 print "Going to 1st coordinate"
 vehicle.airspeed=100
-new_location = LocationGlobal(-34.36227, 155.1667, 40)
+new_location = LocationGlobalRelative(-34.36227, 155.1667, 40)
 vehicle.simple_goto(new_location)
 time.sleep(50)
 print "Completed 1st coordinate"
@@ -57,7 +57,7 @@ print "Completed 1st coordinate"
 #Travel to another coordinate for 50 seconds
 print "Going to 2nd coordinate"
 vehicle.airspeed=10
-new_location2 = LocationGlobal(-80.36227, 62.1667, 40)
+new_location2 = LocationGlobalRelative(-80.36227, 62.1667, 40)
 vehicle.simple_goto(new_location2)
 time.sleep(50)
 print "Completed 2nd coordinate"
